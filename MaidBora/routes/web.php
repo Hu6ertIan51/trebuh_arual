5e2781d06371d718dc2e8f1058ed6bee4fb1f429
Hu6ertIan51 <ian.ogonjo@strathmore.edu>
2023-07-04 10:58:50 +0300

    Done with models and added a login page, also managed to split our views as per employer, worker and admin

--- MaidBora/routes/web.php
+++ MaidBora/routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Employer');
+    return view('login');
 });
-
-//Employer Sign In
-Route::get('user/login', [UserController::class, 'login']);
